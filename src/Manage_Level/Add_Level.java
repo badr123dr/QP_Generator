72f1a0fe209529d19dbb0f738799147738ee194c
badr123dr <jouad324@gmail.com>
2021-07-04 20:11:28 +0100

    add levels and view list of levels

--- src/Manage_Level/Add_Level.java
+++ src/Manage_Level/Add_Level.java
@@ -1,4 +1,62 @@
 package Manage_Level;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+import Database.*;
+
 public class Add_Level {
+    private JPanel manageLevel;
+    private JLabel lb_title;
+    private JLabel lb_name;
+    private JLabel lb_description;
+    private JButton bt_ajouter;
+    private JTextField txt_nameLVL;
+    private JTextField txt_descriptionLVL;
+
+    /*
+    public static void main(String[] args) throws SQLException {
+
+        JFrame frame = new JFrame("Add_Level");
+        frame.setContentPane(new Add_Level().manageLevel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+    */
+    PreparedStatement pst;
+    DatabaseConnection db ;
+
+    public Add_Level() throws SQLException {
+
+        db = DatabaseConnection.getInstance();
+        bt_ajouter.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String level_name,description;
+                level_name = txt_nameLVL.getText();
+                description = txt_descriptionLVL.getText();
+
+                try {
+                    pst = db.connection.prepareStatement("INSERT INTO " +
+                            "level(name_level,description)" +
+                            "VALUES(?,?)");
+                    pst.setString(1, level_name);
+                    pst.setString(2, description);
+                    pst.executeUpdate();
+                    JOptionPane.showMessageDialog(null, "Level added succesefully");
+                    //table_load();
+                    txt_nameLVL.setText("");
+                    txt_descriptionLVL.setText("");
+                    txt_nameLVL.requestFocus();
+                }catch (SQLException e1)
+                {
+                    e1.printStackTrace();
+                }
+
+            }
+        });
+    }
 }
--- src/Manage_Level/View_Level.java
+++ src/Manage_Level/View_Level.java
@@ -1,4 +1,98 @@
 package Manage_Level;
 
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+import Database.*;
+import net.proteanit.sql.DbUtils;
+import Manage_Level.*;
+
 public class View_Level {
+    private JLabel lb_title;
+    private JButton bt_modifier;
+    private JButton bt_supprimer;
+    private JTable level_table;
+    private JPanel levelList;
+    private JScrollPane table_levels;
+
+    public static void main(String[] args) throws SQLException {
+
+        JFrame frame = new JFrame("View_Level");
+        frame.setContentPane(new View_Level().levelList);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+
+    PreparedStatement pst;
+    DatabaseConnection db ;
+
+    public void showTableData(){
+        try
+        {
+            pst = db.connection.prepareStatement("select id_level ID, name_level as 'Level name' ,description Description " +
+                    "from level");
+            ResultSet rs = pst.executeQuery();
+            level_table.setModel(DbUtils.resultSetToTableModel(rs));
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public View_Level() throws SQLException {
+
+        db=DatabaseConnection.getInstance();
+        showTableData();
+
+        bt_modifier.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //frame.dispose();
+                new Update_Level();
+
+                //----ANI WASL HNA BAGHI NA FFICHER JFRAME UPDATE_LEVEL O N3TEHA LES INFOS DYA
+                //---- LEVEL SELECTIONNé F JTABLE
+            }
+        });
+
+        bt_supprimer.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                DefaultTableModel tblModel = (DefaultTableModel) level_table.getModel();
+                if(level_table.getSelectedRowCount()==1){
+                    //if a single row is selected
+                    int i = level_table.getSelectedRow();
+                    String id = tblModel.getValueAt(i,0).toString();
+
+                    try {
+                        pst = db.connection.prepareStatement("DELETE FROM level WHERE id_level =?");
+                        pst.setString(1,id);
+                        pst.executeUpdate();
+
+                        showTableData();
+                        //tblModel.removeRow(level_table.getSelectedRow());
+                        JOptionPane.showMessageDialog(null,"Deleted successfully !");
+
+                    } catch (SQLException | HeadlessException ex) {
+                        JOptionPane.showMessageDialog(null,ex);
+                    }
+
+                }else{
+                    if(level_table.getRowCount()==0){
+                        //if table is empty
+                        JOptionPane.showMessageDialog(null,"Table is empty !!");
+                    }else {
+                        //if table is not empty but any row is selected or multiple rows are selected
+                        JOptionPane.showMessageDialog(null,"Please select a single row to delete !");
+                    }
+                }
+            }
+        });
+    }
 }
